f7410687058802940ad37cae2d93d664ec54bdf1
Apip Akmalul Iman <bangsantri33@gmail.com>
2025-03-04 00:20:57 +0700

    Create Struktur.php

---
+++ Struktur.php
@@ -0,0 +1,9 @@
+apartment_management/
+│── app.py              # Entry point utama Flask
+│── config.py           # Konfigurasi database MySQL
+│── models.py           # Model SQLAlchemy (ORM)
+│── routes.py           # Routing API Flask
+│── templates/          # File HTML (Frontend)
+│── static/             # CSS, JavaScript, Gambar
+│── db.sql              # Skrip database MySQL
+│── requirements.txt    # Dependencies Python
